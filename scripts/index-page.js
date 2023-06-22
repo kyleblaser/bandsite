b39da15e9ad6410a8031982a3896831afe420fac
kyle <kyleblaser.kb@gmail.com>
2023-06-22 19:49:22 -0400

    sprint 2 progress

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,6 +1,5 @@
 /** @format */
 
-// Function to format the date in a numeric format
 function formatDate(dateString) {
   const date = new Date(dateString);
   const day = date.getDate().toString().padStart(2, "0");
@@ -9,57 +8,45 @@ function formatDate(dateString) {
   return `${month}/${day}/${year}`;
 }
 
-// Function to create a comment container element
 function createCommentContainer(name, comment, date) {
-  // Create a new comment container element
   const commentContainer = document.createElement("div");
   commentContainer.classList.add("comment-container");
 
-  // Create a new profile picture element
   const profilePicture = document.createElement("div");
   profilePicture.classList.add("profile-picture");
   const profilePictureImg = document.createElement("img");
-  profilePictureImg.src = "./assets/images/Mohan-muruge.jpg"; // Replace with the actual profile picture URL
+  profilePictureImg.src = "./assets/images/Mohan-muruge.jpg";
   profilePictureImg.alt = "Profile Picture";
   profilePicture.appendChild(profilePictureImg);
 
-  // Create a new comment content element
   const commentContent = document.createElement("div");
   commentContent.classList.add("comment-content");
 
-  // Create a new comment name element
   const commentName = document.createElement("strong");
   commentName.classList.add("comment-name");
   commentName.textContent = name;
 
-  // Create a new comment date element
   const commentDate = document.createElement("span");
   commentDate.classList.add("comment-date");
   commentDate.textContent = formatDate(date);
 
-  // Create a new comment text element
   const commentText = document.createElement("p");
   commentText.classList.add("comment-text");
   commentText.textContent = comment;
 
-  // Append the name and date to the comment content
   commentContent.appendChild(commentName);
   commentContent.appendChild(commentDate);
 
-  // Append the comment text to the comment content
   commentContent.appendChild(commentText);
 
-  // Append the profile picture and comment content to the comment container
   commentContainer.appendChild(profilePicture);
   commentContainer.appendChild(commentContent);
 
   return commentContainer;
 }
 
-// Get the comment list element
 const commentList = document.getElementById("commentList");
 
-// Pre-defined comments
 const prePopulatedComments = [
   {
     name: "Connor Walton",
@@ -81,35 +68,27 @@ const prePopulatedComments = [
   },
 ];
 
-// Loop through the pre-defined comments
 prePopulatedComments.forEach((commentData) => {
   const { name, comment, date } = commentData;
 
-  // Create a comment container element
   const commentContainer = createCommentContainer(name, comment, date);
 
-  // Append the comment container to the comment list
   commentList.insertBefore(commentContainer, commentList.firstChild);
 });
 
-// Handle form submission
 document
   .getElementById("commentForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    // Get the values from the form inputs
     const name = document.getElementById("nameInput").value;
     const comment = document.getElementById("commentInput").value;
     const currentDate = new Date();
 
-    // Create a comment container element for the new comment
     const commentContainer = createCommentContainer(name, comment, currentDate);
 
-    // Append the new comment container to the comment list at the top
     commentList.insertBefore(commentContainer, commentList.firstChild);
 
-    // Clear the form inputs
     document.getElementById("nameInput").value = "";
     document.getElementById("commentInput").value = "";
   });
